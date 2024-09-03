9c294a84cd45c4eff6fdf19867ac411e88279658
Pegbeer <carlosgarciahl@hotmail.com>
2024-09-03 13:14:36 -0600

    Test for mapping results to model class

--- data/src/androidTest/java/me/pegbeer/rickandmortydb/data/GraphQLInstrumentedTest.kt
+++ data/src/androidTest/java/me/pegbeer/rickandmortydb/data/GraphQLInstrumentedTest.kt
@@ -4,19 +4,26 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.apollographql.apollo.ApolloClient
 import com.rickandmortydb.GetAllCharactersQuery
 import kotlinx.coroutines.test.runTest
+import me.pegbeer.rickandmortydb.core.model.Character
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class GraphQLInstrumentedTest {
 
-    @Test
-    fun testGetAllCharacters() = runTest{
-        val apolloClient = ApolloClient.Builder()
+    private lateinit var apolloClient: ApolloClient
+
+    @Before
+    fun setUp(){
+        apolloClient = ApolloClient.Builder()
             .serverUrl("https://rickandmortyapi.com/graphql")
             .build()
+    }
 
-        val response = apolloClient.query(GetAllCharactersQuery()).execute()
+    @Test
+    fun testGetAllCharacters() = runTest{
+        val response = apolloClient.query(GetAllCharactersQuery(1)).execute()
         val characters = response.data?.characters?.results
         assert(characters != null)
         assert(characters!!.isNotEmpty())
@@ -24,5 +31,19 @@ class GraphQLInstrumentedTest {
             println(i?.name)
         }
         println("SIZE: ${characters.size}")
+    }
+
+    @Test
+    fun testMappingAllCharacters() = runTest {
+        val response = apolloClient.query(GetAllCharactersQuery(1)).execute()
+
+        val characters = response.data!!.characters!!.results
+
+        val mappedCharacters = characters!!.map {
+            it!!.let { char ->
+                Character(char.id,char.name,char.image,char.gender)
+            }
+        }
+        assert(mappedCharacters.isNotEmpty())
     }
 }
