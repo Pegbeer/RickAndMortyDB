17c9e364819b3e4c00d21b48cd32a4fcaeb7534a
Pegbeer <carlosgarciahl@hotmail.com>
2024-09-03 12:07:28 -0600

    Test for GetAllCharacters query

--- data/src/androidTest/java/me/pegbeer/rickandmortydb/data/GraphQLInstrumentedTest.kt
+++ data/src/androidTest/java/me/pegbeer/rickandmortydb/data/GraphQLInstrumentedTest.kt
@@ -1,4 +1,28 @@
 package me.pegbeer.rickandmortydb.data
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.apollographql.apollo.ApolloClient
+import com.rickandmortydb.GetAllCharactersQuery
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class GraphQLInstrumentedTest {
+
+    @Test
+    fun testGetAllCharacters() = runTest{
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://rickandmortyapi.com/graphql")
+            .build()
+
+        val response = apolloClient.query(GetAllCharactersQuery()).execute()
+        val characters = response.data?.characters?.results
+        assert(characters != null)
+        assert(characters!!.isNotEmpty())
+        for (i in characters){
+            println(i?.name)
+        }
+        println("SIZE: ${characters.size}")
+    }
 }
