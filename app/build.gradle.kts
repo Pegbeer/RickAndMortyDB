3101aefdfcab068a6ce861029b9577fda053d24a
Pegbeer <carlosgarciahl@hotmail.com>
2024-09-03 10:29:19 -0600

    Upgrade Compose Compiler version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,13 +42,17 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+kotlin{
+    jvmToolchain(11)
 }
 
 dependencies {
